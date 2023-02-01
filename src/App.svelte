a0149729d50b44281d06921036ccc42e3ab85b29
blinry <mail@blinry.org>
2023-02-01 11:21:18 +0100

    Basic functionality works

--- src/App.svelte
+++ src/App.svelte
@@ -1,49 +1,73 @@
 <script>
-    import svelteLogo from "./assets/svelte.svg"
-    import Counter from "./lib/Counter.svelte"
+    let header = ""
+    let data = ""
+
+    async function getData(url) {
+        header = url
+        data = ""
+        window.location.hash = url
+        fetch(url, {
+            headers: {
+                Accept: 'application/ld+json; profile="https://www.w3.org/ns/activitystreams"',
+                "Access-Control-Allow-Origin": "*",
+                mode: "no-cors",
+            },
+        }).then(async (response) => {
+            let json = await response.json()
+            // pretty print
+            data = urlify(JSON.stringify(json, null, 4))
+        })
+    }
+
+    function urlify(text) {
+        let urlRegex = /(https?:\/\/[^\s,"\\]+)/g
+        return text.replace(urlRegex, function (url) {
+            console.log(url)
+            // wrap in tag that, when clicked, calls getData
+            return `<a href="#${url}">${url}</a>`
+        })
+    }
+
+    function hashChange() {
+        getData(window.location.hash.substring(1))
+    }
+
+    // is the hash non-empty?
+    if (window.location.hash) {
+        hashChange()
+    } else {
+        getData("https://chaos.social/users/blinry")
+    }
 </script>
 
+<svelte:window on:hashchange={hashChange} />
+
 <main>
-    <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-            <img src="/vite.svg" class="logo" alt="Vite Logo" />
-        </a>
-        <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-            <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-        </a>
-    </div>
-    <h1>Vite + Svelte</h1>
-
-    <div class="card">
-        <Counter />
-    </div>
-
-    <p>
-        Check out <a
-            href="https://github.com/sveltejs/kit#readme"
-            target="_blank"
-            rel="noreferrer">SvelteKit</a
-        >, the official Svelte app framework powered by Vite!
-    </p>
-
-    <p class="read-the-docs">
-        Click on the Vite and Svelte logos to learn more
-    </p>
+    <input
+        type="text"
+        bind:value={header}
+        on:keydown={(e) => {
+            if (e.key === "Enter") {
+                getData(header)
+            }
+        }}
+    />
+    <div class="json">{@html data}</div>
 </main>
 
 <style>
-    .logo {
-        height: 6em;
-        padding: 1.5em;
-        will-change: filter;
+    input {
+        font-size: 120%;
+        width: 100%;
+        margin-bottom: 1rem;
+        padding: 0.5rem;
     }
-    .logo:hover {
-        filter: drop-shadow(0 0 2em #646cffaa);
-    }
-    .logo.svelte:hover {
-        filter: drop-shadow(0 0 2em #ff3e00aa);
-    }
-    .read-the-docs {
-        color: #888;
+    .json {
+        font-family: monospace;
+        white-space: pre-wrap;
+        text-align: left;
+        background: #eee;
+        border-radius: 1rem;
+        padding: 1rem;
     }
 </style>
